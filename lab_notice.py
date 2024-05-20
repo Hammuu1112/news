5caeaa73855be5609c27a11cf76e325b41218616
Hammuu1112 <97436524+Hammuu1112@users.noreply.github.com>
2024-05-20 20:38:44 +0900

    Update lab_notice.py

--- lab_notice.py
+++ lab_notice.py
@@ -4,7 +4,7 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-from utils import get_content_id_from_url, get_description, WebHookData, post_to_webhook
+from utils import get_content_id_from_url, get_description, get_date, WebHookData, post_to_webhook
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -33,8 +33,11 @@ def lab_notice(web_hook_url: str):
             web_hook_data = WebHookData(title=title, url=url, description=desc, thumbnail=thumbnail, data_type="lab",
                                         date=date)
             post_to_webhook(url=web_hook_url, data=web_hook_data.to_json())
+            dded = get_date()
+        else:
+            added = old_events[content_id]['added'] if 'added' in old_events[content_id] else get_date()
         notices[content_id] = {'title': title, 'date': date, 'url': url, 'thumbnail': thumbnail, 'desc': desc,
-                               'description': description, 'content_id': content_id}
+                               'description': description, 'content_id': content_id, 'added': added}
 
     with open(os.path.join(BASE_DIR, 'news', 'lab_notice.json'), 'w+', encoding='utf-8') as json_file:
         json.dump(notices, json_file, ensure_ascii=False, indent='\t')
