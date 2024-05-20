bd187425d6b0b4e5444f8b0e003d3fb18ae6c7e1
Hammuu1112 <97436524+Hammuu1112@users.noreply.github.com>
2024-05-20 20:43:13 +0900

    Update lab_update.py

--- lab_update.py
+++ lab_update.py
@@ -36,7 +36,7 @@ def lab_update(web_hook_url: str):
             post_to_webhook(url=web_hook_url, data=web_hook_data.to_json())
             dded = get_date()
         else:
-            added = old_events[content_id]['added'] if 'added' in old_events[content_id] else get_date()
+            added = old_updates[content_id]['added'] if 'added' in old_updates[content_id] else get_date()
         updates[content_id] = {'title': title, 'date': date, 'url': url, 'thumbnail': thumbnail, 'desc': desc,
                                'description': description, 'content_id': content_id, 'added': added}
 
