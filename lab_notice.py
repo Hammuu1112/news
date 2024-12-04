f0ba9cb3fb00746467e02b8dcbb29aa0dd58e1ff
Hammuu1112 <97436524+Hammuu1112@users.noreply.github.com>
2024-12-04 21:30:32 +0900

    Update lab_notice.py

--- lab_notice.py
+++ lab_notice.py
@@ -12,19 +12,20 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 def lab_notice(web_hook_url: str):
     with open(os.path.join(BASE_DIR, 'news', 'lab_notice.json'), 'r', encoding='utf-8') as file:
         old_notices: dict = json.load(file)
-    response = requests.request(method='get', url='https://www.global-lab.playblackdesert.com/News/Notice?boardType=1',
+    response = requests.request(method='get', url='https://blackdesert.pearlabyss.com/GlobalLab/ko-KR/News/Notice?_categoryNo=1',
                                 headers={'Accept-Language': 'ko-KR,ko;q=0.9'})
     response.encoding = None
     html = response.content
     soup = BeautifulSoup(html, 'html.parser')
-    data = soup.select('#wrap > div > div.container > article > div.tab_container > div > div.thumb_nail_area > ul')[0]
+    data = soup.select('#wrap > div > section > article > div.board_wrap > ul')[0]
     table = data.findAll('li')
 
     notices = {}
     for item in table:
-        title = item.find_next('strong').find_next('em').next_sibling.split(' (최종 수정')[0].strip()
-        date = item.find_next('strong').find_next('span', {'class', 'date'}).getText()
-        desc = item.find_next('strong').find_next('span', {'class', 'desc'}).getText()
+        # title = item.find_next('strong').find_next('em').next_sibling.split(' (최종 수정')[0].strip()
+        title = item.find_next('p', {'class', 'title'}).getText().split(' (최종 수정')[0].strip()
+        date = item.find_next('div', {'class', 'info'}).find_next('span', {'class', 'date'}).getText()
+        desc = item.find_next('div', {'class', 'info'}).find_next('span', {'class', 'thum_cate'}).getText()
         url = item.find_next('a').get('href')
         content_id = get_content_id_from_url(url)
         thumbnail = item.find_next('img').get('src')
